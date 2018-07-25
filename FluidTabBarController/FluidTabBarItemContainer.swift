8666d7d6c7a3cda493b32dc58ee49b05a917c18f
Hubert Kuczynski <hubert.kuczynski@10clouds.com>
2018-07-25 16:07:02 +0200

    Add MIT licence to all file headers

--- FluidTabBarController/FluidTabBarItemContainer.swift
+++ FluidTabBarController/FluidTabBarItemContainer.swift
@@ -2,8 +2,28 @@
 //  FluidTabBarItemContainer.swift
 //  FluidTabBarController
 //
-//  Created by Hubert Kuczyński on 10/07/2018.
-//  Copyright © 2018 10Clouds Sp. z o.o. All rights reserved.
+//  Created by Vincent Li on 2017/2/8.
+//  Copyright (c) 2013-2018 ESTabBarController (https://github.com/eggswift/ESTabBarController)
+//
+//  Modified by Hubert Kuczyński on 09/07/2018.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
 //
 
 import UIKit
